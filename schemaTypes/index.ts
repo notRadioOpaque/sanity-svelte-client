180a41f8c9d2c23c5e1bd80f6b816f1cd9b5f087
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-24 08:25:45 +0100

    feat: update schemaTypes to include about, blogPost, experience, and project schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import about from '../schemas/about'
+import blogPost from '../schemas/blogPost'
+import experience from '../schemas/experience'
+import project from '../schemas/project'
+
+export const schemaTypes = [project, about, experience, blogPost]
